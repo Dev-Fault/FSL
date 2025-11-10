4e181797b750332bd41925378de2bf3e33d6fbb8
Dev-Fault <devfaultapps@gmail.com>
2025-11-10 10:42:59 -0500

    separated standard library code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use core::fmt;
 use std::{ops::Range, sync::Arc};
 
+mod standard_library;
+
 type Error = String;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -54,6 +56,7 @@ impl ArgRule {
 }
 
 type Executor = Box<dyn Fn(Vec<Value>) -> Result<Value, Error> + Send + Sync>;
+
 struct Command {
     label: String,
     arg_rules: Vec<ArgRule>,
@@ -317,52 +320,10 @@ impl From<Arc<Command>> for Value {
     }
 }
 
-fn add(values: Vec<Value>) -> Result<Value, Error> {
-    if values.contains_float() {
-        let mut sum: f64 = 0.0;
-        for value in values {
-            sum = sum + value.as_float()?;
-        }
-        Ok(Value::Float(sum))
-    } else {
-        let mut sum: i64 = 0;
-        for value in values {
-            sum = sum + value.as_int()?;
-        }
-        Ok(Value::Int(sum))
-    }
-}
-
-fn sub(values: Vec<Value>) -> Result<Value, Error> {
-    if values.contains_float() {
-        let mut diff = values[0].as_float()?;
-        for value in &values[1..values.len()] {
-            diff = diff - value.as_float()?;
-        }
-        Ok(Value::Float(diff))
-    } else {
-        // Should never panic as length and types have already been checked
-        let mut diff = values[0].as_int()?;
-        for value in &values[1..values.len()] {
-            diff = diff - value.as_int()?;
-        }
-        Ok(Value::Int(diff))
-    }
-}
-
-fn repeat(values: Vec<Value>) -> Result<Value, Error> {
-    let repetitions = values[0].as_int()?;
-    let command = values[1].as_command()?;
-    let mut final_value = Value::None;
-    for i in 0..repetitions {
-        final_value = command.execute()?;
-    }
-
-    Ok(final_value)
-}
-
 #[cfg(test)]
 mod tests {
+
+    use crate::standard_library::*;
 
     use super::*;
 
